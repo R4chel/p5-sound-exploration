42109ec80199ddd8da5ca0b4c4969995c7ebe7d8
R4chel <rae84@cornell.edu>
2022-08-18 14:01:10 -0400

    exploration

--- art.js
+++ art.js
@@ -1,18 +1,56 @@
 function Art(config, ranges) {
     this.config = config;
+    this.t = 0;
 
     this.draw = function(soundwave, amplitude) {
-        let sections = 2;
+        this.soundAnalysis(soundwave);
+        let sections = 5;
         let buffer = 5;
         let sectionHeight = height / sections - buffer;
-        
-        let section =0;
-        drawGraph(soundwave, color(0), color("blue"), section*(sectionHeight+buffer), section*(sectionHeight+buffer) +sectionHeight);
+
+        let section = 0;
+        drawGraph(soundwave, color(50 * section), color("blue"), section * (sectionHeight + buffer), section * (sectionHeight + buffer) + sectionHeight);
         section++;
 
-        drawLogGraph(soundwave, color(50), color("magenta"), section*(sectionHeight+buffer), section*(sectionHeight+buffer) +sectionHeight);
+        drawLogGraph(soundwave, color(50 * section), color("magenta"), section * (sectionHeight + buffer), section * (sectionHeight + buffer) + sectionHeight);
+        section++;
+
+        drawGraph(this.avgs, color(50 * section), color("green"), section * (sectionHeight + buffer), section * (sectionHeight + buffer) + sectionHeight);
+        section++;
+
+        drawGraph(this.ranges, color(50 * section), color("purple"),section * (sectionHeight + buffer), section * (sectionHeight + buffer) + sectionHeight);
+        section++;
     };
 
+
+    this.avgs = [];
+    this.ranges = [];
+    this.runningAvg = 0;
+    this.runningRangeAvg = 0;
+
+    this.soundAnalysis = function(soundwave) {
+        let sampleMin = 0;
+        let sampleMax = 0;
+        let sampleSum = 0;
+        for (let i = 0; i < soundwave.length; i++) {
+            let value = soundwave[i];
+            sampleMin = min(sampleMin, value);
+            sampleMax = max(sampleMax, value);
+            sampleSum += abs(value);
+        }
+        let sampleAvg = 2 * sampleSum / soundwave.length;
+        let range = sampleMax - sampleMin;
+
+        this.avgs.push(sampleAvg);
+        this.ranges.push(range);
+        while (this.avgs.length > this.config.windowSize) {
+            this.avgs.shift();
+            this.ranges.shift();
+        }
+        this.runningAvg = this.avgs.reduce((acc, x) => acc + x, 0) / this.avgs.length;
+        this.runningRangeAvg = this.ranges.reduce((acc, x) => acc + x, 0) / this.ranges.length;
+
+    };
 
     this.keyPress = function(key) {
         switch (key) {
@@ -47,31 +85,31 @@ function Art(config, ranges) {
     this.reset();
 }
 
-function drawGraph(data, bg, fg, top, bottom){
+function drawGraph(data, bg, fg, top, bottom) {
     noStroke();
     fill(bg);
-    rect(0, top, width, bottom-top);
+    rect(0, top, width, bottom - top);
     noFill();
     stroke(fg);
     beginShape();
     for (let i = 0; i < data.length; i++) {
         let x = map(i, 0, data.length, 0, width);
-        let y = map(data[i],-1, 1, bottom, top);
+        let y = map(data[i], -1, 1, bottom, top);
         curveVertex(x, y);
     }
     endShape();
 }
 
-function drawLogGraph(data, bg, fg, top, bottom){
+function drawLogGraph(data, bg, fg, top, bottom) {
     noStroke();
     fill(bg);
-    rect(0, top, width, bottom-top);
+    rect(0, top, width, bottom - top);
     noFill();
     stroke(fg);
     beginShape();
     for (let i = 0; i < data.length; i++) {
         let x = map(log(i), 0, log(data.length), 0, width);
-        let y = map(data[i],-1, 1, bottom, top);
+        let y = map(data[i], -1, 1, bottom, top);
         curveVertex(x, y);
     }
     endShape();
--- config.js
+++ config.js
@@ -3,11 +3,13 @@ function Config({
     debug,
     canvasWidth,
     canvasHeight,
+    windowSize,
 }) {
 
     this.debug = debug === undefined ? false : debug;
     this.canvasWidth = canvasWidth === undefined ? (debug ? 200 : windowWidth) : canvasWidth;
     this.canvasHeight = canvasHeight === undefined ? (debug ? 200 : windowHeight) : canvasHeight;
+    this.windowSize = windowSize === undefined ? 20: windowSize;
     this.seed = seed === undefined ? (debug ? 1 : seed) : seed;
     this.setSeed = function() {
         if (this.seed === undefined) {
